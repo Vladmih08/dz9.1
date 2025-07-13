c03cc7bff2ed5b108ff1069394a085ac447642fa
vladmшр08 <vladmih2008@gmail.com>
2025-07-13 20:17:32 +0300

    Перша версія коду, я сказав б що вона за якимось причинами пішла дуже погано. Можливо, найскладніша за увесь час. Але мені дуже допомогли "підказки" від самоі програми, котрі я підтверджував на таб =)

---
+++ dz9.1.py
@@ -0,0 +1,18 @@
+def popular_words (text, words):
+    text = text.lower()
+    text = text.split()
+    kolvo_raz = []
+    for slovo in words:
+        kolvo = 0
+        for a in text:
+            if a == slovo:
+                kolvo += 1
+        kolvo_raz.append(kolvo)
+    otvet = {}
+    index = 0
+    for slovo in words:
+        otvet[slovo] = kolvo_raz[index]
+        index += 1
+    return otvet
+assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }, 'Test1'
+print('OK')
